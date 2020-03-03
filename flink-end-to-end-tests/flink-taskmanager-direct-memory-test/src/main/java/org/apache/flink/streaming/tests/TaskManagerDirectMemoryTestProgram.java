0cc9cfe8d9d339535792dcd30c2d5f4e9fc32f55
Gao Yun <yungao.gy@alibaba-inc.com>
2020-03-03 18:21:32 +0800

    Add the taskmanager direct memory test

---
+++ flink-end-to-end-tests/flink-taskmanager-direct-memory-test/src/main/java/org/apache/flink/streaming/tests/TaskManagerDirectMemoryTestProgram.java
@@ -0,0 +1,160 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.tests;
+
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.configuration.ConfigOption;
+import org.apache.flink.configuration.ConfigOptions;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.apache.flink.util.Preconditions.checkArgument;
+
+/**
+ * Test program.
+ */
+public class TaskManagerDirectMemoryTestProgram {
+	private static final Logger LOG = LoggerFactory.getLogger(TaskManagerDirectMemoryTestProgram.class);
+
+	private static ConfigOption<Long> RATE = ConfigOptions
+		.key("test.rate")
+		.defaultValue(10000L)
+		.withDescription("The number of record sent per second by one map task.");
+
+	private static ConfigOption<Long> TOTAL_RECORDS = ConfigOptions
+		.key("test.total_records")
+		.defaultValue(30000L)
+		.withDescription("The number of records sent by each map task.");
+
+	private static ConfigOption<Integer> RECORD_LENGTH = ConfigOptions
+		.key("test.record_length")
+		.defaultValue(2048)
+		.withDescription("The length of record.");
+
+	private static ConfigOption<Integer> MAP_PARALLELISM = ConfigOptions
+		.key("test.map_parallelism")
+		.defaultValue(1)
+		.withDescription("The number of map tasks.");
+
+	private static ConfigOption<Integer> REDUCE_PARALLELISM = ConfigOptions
+		.key("test.reduce_parallelism")
+		.defaultValue(1)
+		.withDescription("The number of reduce tasks.");
+
+	public static void main(String[] args) throws Exception {
+		// parse the parameters
+		final ParameterTool params = ParameterTool.fromArgs(args);
+
+		final long rate = params.getLong(RATE.key(), RATE.defaultValue());
+		final long totalRecords = params.getLong(TOTAL_RECORDS.key(), TOTAL_RECORDS.defaultValue());
+		final int recordLength = params.getInt(RECORD_LENGTH.key(), RECORD_LENGTH.defaultValue());
+		final int mapParallelism = params.getInt(MAP_PARALLELISM.key(), MAP_PARALLELISM.defaultValue());
+		final int reduceParallelism = params.getInt(REDUCE_PARALLELISM.key(), REDUCE_PARALLELISM.defaultValue());
+
+		checkArgument(rate > 0, "The sending rate should be positive, but it is {}", rate);
+		checkArgument(totalRecords > 0,
+			"The number of records to send should be positive, but it is {}",
+			totalRecords);
+		checkArgument(recordLength > 0,
+			"The record length should be positive, but it is {}",
+			recordLength);
+		checkArgument(mapParallelism > 0,
+			"The number of map tasks should be positive, but it is {}",
+			mapParallelism);
+		checkArgument(reduceParallelism > 0,
+			"The number of reduce tasks should be positve, but it is {}",
+			reduceParallelism);
+
+		byte[] bytes = new byte[recordLength];
+		for (int i = 0; i < recordLength; ++i) {
+			bytes[i] = 'a';
+		}
+		String str = new String(bytes);
+
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		env.addSource(new StringSourceFunction(rate, totalRecords, str))
+			.setParallelism(mapParallelism)
+			.rebalance()
+			.addSink(new DummySink())
+			.setParallelism(reduceParallelism);
+
+		// execute program
+		env.execute("TaskManager Direct Memory Test");
+	}
+
+	public static class StringSourceFunction extends RichParallelSourceFunction<String> {
+		private static final long serialVersionUID = 1L;
+
+		private volatile boolean isRunning;
+
+		private final long sleepIntervalInNanos;
+
+		private long remainingRecords;
+
+		private final String str;
+
+		public StringSourceFunction(long rate, long totalRecords, String str) {
+			if (rate >= 1e7) {
+				this.sleepIntervalInNanos = 0;
+			} else {
+				this.sleepIntervalInNanos = 1_000_000_000L / rate;
+			}
+
+			this.remainingRecords = totalRecords;
+			this.str = str;
+		}
+
+		@Override
+		public void open(Configuration parameters) {
+			isRunning = true;
+		}
+
+		@Override
+		public void run(SourceContext<String> ctx) throws Exception {
+			while (isRunning && remainingRecords > 0) {
+				ctx.collect(str);
+				remainingRecords--;
+
+				if (sleepIntervalInNanos > 0) {
+					TimeUnit.NANOSECONDS.sleep(sleepIntervalInNanos);
+				}
+			}
+		}
+
+		@Override
+		public void cancel() {
+			isRunning = false;
+		}
+	}
+
+	private static class DummySink extends RichSinkFunction<String> {
+
+		@Override
+		public void invoke(String value, Context context) throws Exception {
+			// Do nothing.
+		}
+	}
+}
---
+++ flink-end-to-end-tests/test-scripts/test_taskmanager_direct_memory.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+################################################################################
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+source "$(dirname "$0")"/common.sh
+
+MORE_TMS=$1
+TOTAL_RECORDS=$2
+
+echo "MORE_TMS = ${MORE_TMS}, max count = ${TOTAL_RECORDS}"
+
+TEST=flink-taskmanager-direct-memory-test
+TEST_PROGRAM_NAME=TaskManagerDirectMemoryTestProgram
+TEST_PROGRAM_JAR=${END_TO_END_DIR}/$TEST/target/$TEST_PROGRAM_NAME.jar
+
+set_config_key "akka.ask.timeout" "60 s"
+set_config_key "web.timeout" "60000"
+
+set_config_key "taskmanager.memory.process.size" "1024m" # 1024Mb x 5TMs = 5Gb total heap
+
+set_config_key "taskmanager.memory.managed.size" "8" # 8Mb
+set_config_key "taskmanager.memory.network.min" "128mb"
+set_config_key "taskmanager.memory.network.max" "128mb"
+set_config_key "taskmanager.memory.jvm-metaspace.size" "64m"
+
+set_config_key "taskmanager.numberOfTaskSlots" "20" # 20 slots per TM
+set_config_key "taskmanager.network.netty.num-arenas" "1"
+set_config_key "taskmanager.memory.framework.off-heap.size" "20m"
+
+start_cluster # this also starts 1TM
+start_taskmanagers ${MORE_TMS} # 1TM + 4TM = 5TM a 20 slots = 100 slots
+
+# This call will result in a deployment with state meta data of 100 x 100 x 40 union states x each 40 entries.
+# We can scale up the numbers to make the test even heavier.
+$FLINK_DIR/bin/flink run ${TEST_PROGRAM_JAR} \
+--test.map_parallelism $(echo "${MORE_TMS} * 20" | bc) \
+--test.reduce_parallelism 20 \
+--test.rate 10000000 \
+--test.record_length 2048 \
+--test.total_records ${TOTAL_RECORDS}
