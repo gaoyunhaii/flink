e21906a8a2ffd4d977181128e2ece57682a979e5
Gao Yun <yungao.gy@alibaba-inc.com>
2020-03-10 08:56:03 +0800

    Fix e2e test

--- flink-end-to-end-tests/test-scripts/flink-netty-shuffle-memory-control-test.sh
+++ flink-end-to-end-tests/test-scripts/flink-netty-shuffle-memory-control-test.sh
@@ -19,29 +19,22 @@
 
 source "$(dirname "$0")"/common.sh
 
-TEST=flink-taskmanager-direct-memory-test
-TEST_PROGRAM_NAME=TaskManagerDirectMemoryTestProgram
+TEST=flink-netty-shuffle-memory-control-test
+TEST_PROGRAM_NAME=NettyShuffleMemoryControlTestProgram
 TEST_PROGRAM_JAR=${END_TO_END_DIR}/$TEST/target/$TEST_PROGRAM_NAME.jar
 
-set_config_key "akka.ask.timeout" "60 s"
-set_config_key "web.timeout" "60000"
-
-set_config_key "taskmanager.memory.process.size" "1536m"
-
-set_config_key "taskmanager.memory.managed.size" "8" # 8Mb
-set_config_key "taskmanager.memory.network.min" "256mb"
-set_config_key "taskmanager.memory.network.max" "256mb"
-set_config_key "taskmanager.memory.jvm-metaspace.size" "64m"
+set_config_key "taskmanager.memory.process.size" "512m"
+set_config_key "taskmanager.memory.network.min" "128mb"
+set_config_key "taskmanager.memory.network.max" "128mb"
 
 set_config_key "taskmanager.numberOfTaskSlots" "20" # 20 slots per TM
 set_config_key "taskmanager.network.netty.num-arenas" "1" # Use only one arena for each TM
 set_config_key "taskmanager.memory.framework.off-heap.size" "20m" # One chunk (16M) and some additional consumption
 
 start_cluster # this also starts 1TM
-start_taskmanagers 4 # 1TM + 4TM = 5TM a 20 slots = 100 slots
+start_taskmanagers 4 # 1TM + 4TM = 5TM * 20 slots = 100 slots
 
 $FLINK_DIR/bin/flink run ${TEST_PROGRAM_JAR} \
 --test.map_parallelism 80 \
 --test.reduce_parallelism 20 \
---test.record_length 2048 \
 --test.running_time_in_seconds 120
