fcbf9dc2b5f42de0f65111f428677e8a719be8c7
Gao Yun <gaoyunhenhao@gmail.com>
2020-05-27 10:46:46 +0800

    Change listener to set

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/api/functions/sink/filesystem/BucketsTest.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/api/functions/sink/filesystem/BucketsTest.java
@@ -324,7 +324,6 @@ public class BucketsTest {
 				new DefaultBucketFactoryImpl<>(),
 				new RowWiseBucketWriter<>(FileSystem.get(path.toUri()).createRecoverableWriter(), new SimpleStringEncoder<>()),
 				DefaultRollingPolicy.builder().build(),
-				null,
 				2,
 				OutputFileConfig.builder().build()
 		);
@@ -452,19 +451,23 @@ public class BucketsTest {
 	private static Buckets<String, String> createBuckets(
 			final Path basePath,
 			final RollingPolicy<String, String> rollingPolicy,
-			@Nullable final BucketLifeCycleListener<String, String> bucketLifeCycleListener,
+			final BucketLifeCycleListener<String, String> bucketLifeCycleListener,
 			final int subtaskIdx,
 			final OutputFileConfig outputFileConfig) throws IOException {
-		return new Buckets<>(
+		Buckets<String, String> buckets = new Buckets<>(
 				basePath,
 				new TestUtils.StringIdentityBucketAssigner(),
 				new DefaultBucketFactoryImpl<>(),
 				new RowWiseBucketWriter<>(FileSystem.get(basePath.toUri()).createRecoverableWriter(), new SimpleStringEncoder<>()),
 				rollingPolicy,
-				bucketLifeCycleListener,
 				subtaskIdx,
-				outputFileConfig
-		);
+				outputFileConfig);
+
+		if (bucketLifeCycleListener != null) {
+			buckets.setBucketLifeCycleListener(bucketLifeCycleListener);
+		}
+
+		return buckets;
 	}
 
 	private static Buckets<String, String> restoreBuckets(
